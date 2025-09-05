faad1bfeeae6e48bdf79b554bd2cb8c3b9e51a6a
Radheyshyam <radheyshyam0987654@gmail.com>
2025-09-05 20:58:42 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#calculatorrrrrrrrrrrrrrrrrrrrrrrrrrr hehehehehehehehe(spiderman waali aawaj mey)
+print('calculator chalaega hain')
+a=int(input('pehla no daal'))
+b=int(input('dooja no daal'))
+print('inme se kuch likh le * / - +')
+c=input('kya krne ka hai')
+
+if c=='+':
+    print(a+b)
+elif c=='-':
+    print(a-b)
+elif c=='*':
+    print(a*b)
+elif c=='/':
+    print(a/b)
+else:
+    print('kyu majak krra hai sahi waala sign likh na')
+    
